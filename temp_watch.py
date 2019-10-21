0142b310e12775690d8bd2928ab5a45893363c2e
Yokinoshita Yokino <45240010+Yokinshita@users.noreply.github.com>
2019-10-21 21:56:39 +0800

    format the code

--- temp_watch.py
+++ temp_watch.py
@@ -8,8 +8,8 @@ if os.path.isfile('temp_record.csv'):
     pass
 else:
     with open('temp_record.csv', 'w') as f:
-            writer = csv.writer(f, delimiter=',')
-            writer.writerow(['time', 'temp', 'RAM'])
+        writer = csv.writer(f, delimiter=',')
+        writer.writerow(['time', 'temp', 'RAM'])
 
 
 # Return RAM information (unit=kb) in a list
@@ -34,7 +34,7 @@ def getCPUtemperature():
     return (res.replace("temp=", "").replace("'C\n", ""))
 
 
-def main(intermitten,is_print):
+def main(intermitten, is_print):
     while True:
         tm = '{}.{}.{} {}:{}:{}'.format(
             time.localtime()[0],
@@ -47,16 +47,17 @@ def main(intermitten,is_print):
         cputemp = getCPUtemperature()
         RAMusage = getRAMinfo()
         if is_print is True:
-            print('time {} temp {} RAMusage {} '.format(tm,cputemp,'%.0f%%'%(RAMusage*100)))
+            print('time {} temp {} RAMusage {} '.format(
+                tm, cputemp, '%.0f%%' % (RAMusage * 100)))
         time.sleep(intermitten)
         with open('temp_record.csv', 'a+') as f:
             writer = csv.writer(f, delimiter=',')
-            writer.writerow([tm, cputemp, '%.0f%%'%(RAMusage*100)])
+            writer.writerow([tm, cputemp, '%.0f%%' % (RAMusage * 100)])
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     try:
-        opts,args=getopt(sys.argv[1:],'hpt:',['help'])
+        opts, args = getopt(sys.argv[1:], 'hpt:', ['help'])
     except getopt.GetoptError:
         is_print = True
         intermitten = 15
@@ -64,17 +65,18 @@ if __name__=="__main__":
         print('Commands:python temp_watch.py [-t time] [-p] ')
     else:
         if opts is not None:
-            is_print=True
-            intermitten=15
+            is_print = True
+            intermitten = 15
         for opt in opts:
             if opt[0] == '-p':
                 is_print = True
             elif opt[0] == '-t':
                 intermitten = int(opt[1])
-            elif opt[0] in ('-h','--help'):
-		print('display and record Cpu tempature and RAM use percent at certain time interval')
-		print('python temp_watch.py [-p] [-t] <interval>')
-		#exit the script
-		sys.exit(0)
-        
-    main(intermitten,is_print)
+            elif opt[0] in ('-h', '--help'):
+                print(
+                    'display and record Cpu tempature and RAM use percent at certain time interval'
+                )
+                print('python temp_watch.py [-p] [-t] <interval>')
+                sys.exit(0)
+
+    main(intermitten, is_print)
